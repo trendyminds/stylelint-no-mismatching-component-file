d1635a53e5e79db8ca18c8094a7a7db38dd23aca
Aaron Bushnell <aaron.bushnell@gmail.com>
2017-07-28 01:42:46 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+var stylelint = require("stylelint");
+var _ = require("lodash");
+
+var ruleName = "tmi/no-mismatching-component-file"
+
+module.exports = stylelint.createPlugin(ruleName, function (enabled) {
+  return function (root, result) {
+    var validOptions = stylelint.utils.validateOptions(result, ruleName, {
+      actual: enabled,
+      possible: [
+        true,
+        false
+      ]
+    })
+
+    if (!validOptions) { return }
+
+    /**
+     * SUPER lazy. This checks to see if we're in the "components" directory because these
+     * rules shouldn't apply to any other directory. I might change this someday.
+     * ...maybe.
+     */
+    var path = result.opts.from.split('/');
+    if (path[path.length - 3] !== 'components') { return }
+
+    var fileName = result.opts.from.match(/_(.*)/g)[0].replace(/_/g, '.').replace(/\.[^/.]+$/g, '');
+
+    root.walkRules(function (statement) {
+      if (
+        statement.parent.type === 'root' &&
+        _.values(statement.parent.indexes)[0] === 0 &&
+        fileName !== statement.selector
+      ) {
+        stylelint.utils.report({
+          ruleName: ruleName,
+          result: result,
+          node: statement,
+          message: `Your selector does not match this module's file name.`
+        });
+      }
+    })
+  }
+})
+
+module.exports.ruleName = ruleName
